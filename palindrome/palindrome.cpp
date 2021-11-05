be8e1c196eef564110dc74021ced375fb848336f
Harun <harunsdemiroglu@hotmail.com>
2021-11-05 11:34:16 +0300

    add palindrome

---
+++ palindrome/palindrome.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+	
+    int a, reversed=0, b;  
+    a = x;
+    while (a != 0) {
+    	b = a % 10;
+    	reversed = reversed*10+b;
+    	a/=10;
+	}
+	bool isPalindrome = (x == reversed);
+}
+
+int main() {
+	
+    int n;
+    cout << "Palindrome?: ";
+    cin >>n;
+    
+    if(isPalindrome(n)) {
+        cout <<n<<" is a palindrome" << endl;
+    }
+    else {
+        cout <<n<<" is NOT a palindrome" << endl;
+    }
+    
+    char c;
+    cout << "Terminate?(y/n): ";
+    cin >> c;
+    char terminate = c;
+    
+    switch (terminate) {	
+    	case 'y':{
+    		return 0;
+			break;
+		}
+		
+		case 'n':{
+			return main();
+			break;
+		}	
+	}
+}
+
+
+

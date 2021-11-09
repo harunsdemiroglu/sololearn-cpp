fcbda7ec6a83ee95a97bfd2cfe57051571368054
Harun <harunsdemiroglu@hotmail.com>
2021-11-09 14:32:50 +0300

    edit palindrome.cpp

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -13,19 +13,35 @@ bool isPalindrome(int x) {
 	bool isPalindrome = (x == reversed);
 }
 
+int test(); 
+
 int main() {
-	
     int n;
 	cout << "Enter an integer: ";
-    cin>>n;
-    
-    if(isPalindrome(n)) {
-        cout <<n<<" is a palindrome" << endl;
-    }
-    else {
-        cout <<n<<" is not a palindrome" << endl;
-    }
-    
+
+    while (!(cin >> n)) {
+    	cout << "Error: Invalid character." << endl;
+    	cin.clear();
+    	cin.ignore(123, '\n');
+    	return main();
+	}
+	if (n < 10) {
+    	cout << "Please enter 2 digit integer." << endl;
+    	return main();
+	} 
+	else { 
+		if (isPalindrome(n)) {
+        	cout <<n<<" is a palindrome" << endl;
+    	}
+    	else {
+        	cout <<n<<" is not a palindrome" << endl;
+    	}
+    	return test();
+	}
+    return test();
+}
+
+int test() {
     char c;
     cout << "Terminate?(y/n): ";
     cin>>c;
@@ -36,13 +52,15 @@ int main() {
     		return 0;
 			break;
 		}
-		
 		case 'n':{
 			return main();
 			break;
-		}	
+		}
+		default:{
+			cout << "Please enter valid answer. (y/n): " << endl;
+			return test();
+			break;
+		}
 	}
+	return main();
 }
-
-
-
